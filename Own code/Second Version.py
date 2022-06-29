823ab4d43f73c005ade609271b72d13e234ad149
Rick van Oosterhout <rick.v.oosterhout@gmail.com>
2022-06-29 16:58:17 +0200

    Added Old AI Models
    -Added the older AI models to the repositoy

---
+++ Own code/Second Version.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+
+from math import prod
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+
+'''
+Spatial Tiny-yolo example
+  Performs inference on RGB camera and retrieves spatial location coordinates: x,y,z relative to the center of depth map.
+  Can be used for tiny-yolo-v3 or tiny-yolo-v4 networks
+'''
+
+# Get argument first
+nnBlobPath = str((Path(__file__).parent / Path('../models/yolo-v4-tiny-tf_openvino_2021.4_6shave.blob')).resolve().absolute())
+if 1 < len(sys.argv):
+    arg = sys.argv[1]
+    if arg == "yolo3":
+        nnBlobPath = str((Path(__file__).parent / Path('../models/yolo-v3-tiny-tf_openvino_2021.4_6shave.blob')).resolve().absolute())
+    elif arg == "yolo4":
+        nnBlobPath = str((Path(__file__).parent / Path('../models/yolo-v4-tiny-tf_openvino_2021.4_6shave.blob')).resolve().absolute())
+    else:
+        nnBlobPath = arg
+else:
+    print("Using Tiny YoloV4 model. If you wish to use Tiny YOLOv3, call 'tiny_yolo.py yolo3'")
+
+if not Path(nnBlobPath).exists():
+    import sys
+    raise FileNotFoundError(f'Required file/s not found, please run "{sys.executable} install_requirements.py"')
+
+# Tiny yolo v3/4 label texts
+labelMap = [
+    "person",         "bicycle",    "car",           "motorbike",     "aeroplane",   "bus",           "train",
+    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",   "parking meter", "bench",
+    "bird",           "cat",        "dog",           "horse",         "sheep",       "cow",           "elephant",
+    "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",    "handbag",       "tie",
+    "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball", "kite",          "baseball bat",
+    "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",      "wine glass",    "cup",
+    "fork",           "knife",      "spoon",         "bowl",          "banana",      "apple",         "sandwich",
+    "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",       "donut",         "cake",
+    "chair",          "sofa",       "pottedplant",   "bed",           "diningtable", "toilet",        "tvmonitor",
+    "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",  "microwave",     "oven",
+    "toaster",        "sink",       "refrigerator",  "book",          "clock",       "vase",          "scissors",
+    "teddy bear",     "hair drier", "toothbrush"
+]
+
+
+# Change Parameters
+nn_preview_resolution = (1024, 576)
+nn_resolution = (416, 416)
+IOU = 0.3
+CONF = 0.3
+
+
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+spatialDetectionNetwork = pipeline.create(dai.node.YoloSpatialDetectionNetwork)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+xoutNN = pipeline.create(dai.node.XLinkOut)
+xoutBoundingBoxDepthMapping = pipeline.create(dai.node.XLinkOut)
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+
+xoutRgb.setStreamName("rgb")
+xoutNN.setStreamName("detections")
+xoutBoundingBoxDepthMapping.setStreamName("boundingBoxDepthMapping")
+xoutDepth.setStreamName("depth")
+
+# Properties
+camRgb.setPreviewSize(nn_preview_resolution)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+
+
+# Squeeze the frame
+manip = pipeline.create(dai.node.ImageManip)
+manip.setMaxOutputFrameSize(prod(nn_resolution) * 3) # 416x416x3
+manip.initialConfig.setResizeThumbnail(nn_resolution)
+camRgb.preview.link(manip.inputImage)
+camRgb.preview.link(xoutRgb.input)
+
+
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+# setting node configs
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+
+spatialDetectionNetwork.setBlobPath(nnBlobPath)
+spatialDetectionNetwork.setConfidenceThreshold(CONF)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+# Yolo specific parameters
+spatialDetectionNetwork.setNumClasses(80)
+spatialDetectionNetwork.setCoordinateSize(4)
+spatialDetectionNetwork.setAnchors(np.array([10,14, 23,27, 37,58, 81,82, 135,169, 344,319]))
+spatialDetectionNetwork.setAnchorMasks({ "side26": np.array([1,2,3]), "side13": np.array([3,4,5]) })
+spatialDetectionNetwork.setIouThreshold(IOU)
+
+# Linking
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+manip.out.link(spatialDetectionNetwork.input)
+
+spatialDetectionNetwork.out.link(xoutNN.input)
+spatialDetectionNetwork.boundingBoxMapping.link(xoutBoundingBoxDepthMapping.input)
+
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
+spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
+
+# Function to show the relative position of a mouse press in the rgb view
+def showPos(event, x, y, flags, param, height, width, layers):
+    # check to see if the left mouse button was pressed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(round(x/width, 2), round(y/height, 2))
+	# check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        print(round(x/width, 2), round(y/height, 2))
+
+
+width, height = nn_preview_resolution
+screen_space_rel = height / width
+black_bar_rel = (1 - screen_space_rel) / 2
+
+def correctBlackBars(y):
+    return (y - black_bar_rel) / screen_space_rel
+
+init_iter = True
+
+
+# Connect to device and start pipeline
+with dai.Device(pipeline, usb2Mode=True) as device:
+
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+    xoutBoundingBoxDepthMappingQueue = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=4, blocking=False)
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+    color = (255, 255, 255)
+
+    while True:
+        inPreview = previewQueue.get()
+        inDet = detectionNNQueue.get()
+        depth = depthQueue.get()
+
+        frame = inPreview.getCvFrame()
+        depthFrame = depth.getFrame() # depthFrame values are in millimeters
+
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        detections = inDet.detections
+        if len(detections) != 0:
+            boundingBoxMapping = xoutBoundingBoxDepthMappingQueue.get()
+            roiDatas = boundingBoxMapping.getConfigData()
+
+            for roiData in roiDatas:
+                roi = roiData.roi
+                roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
+                topLeft = roi.topLeft()
+                bottomRight = roi.bottomRight()
+                xmin = int(topLeft.x)
+                ymin = int(topLeft.y)
+                xmax = int(bottomRight.x)
+                ymax = int(bottomRight.y)
+
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+
+
+        # If the frame is available, draw bounding boxes on it and show the frame      
+        height, width, _ = frame.shape
+        for detection in detections:
+            # Denormalize bounding box
+            x1 = int(detection.xmin * width)
+            x2 = int(detection.xmax * width)
+
+            y1 = int(correctBlackBars(detection.ymin) * height)
+            y2 = int(correctBlackBars(detection.ymax) * height)
+
+            try:
+                label = labelMap[detection.label]
+            except:
+                label = detection.label
+
+
+            coords = detection.spatialCoordinates
+            x,y,z = int(coords.x), int(coords.y), int(coords.z)
+            if x < -150:
+                pos_color = (255,0,0)
+            elif x > 150:
+                pos_color = (0,255,0)
+            else:
+                pos_color = (255,255,0)
+
+            if label == "person" and 0 < z < 2000:
+                det_color = (0,0,255)
+                thickness = cv2.FONT_HERSHEY_DUPLEX
+            else:
+                det_color = color
+                thickness = cv2.FONT_HERSHEY_SIMPLEX
+
+
+            cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"X: {x} mm", (x1 + 10, y1 + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, pos_color)
+            cv2.putText(frame, f"Z: {z} mm", (x1 + 10, y1 + 60), cv2.FONT_HERSHEY_TRIPLEX, 0.5, det_color)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), det_color, thickness)
+
+        cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+        cv2.imshow("depth", depthFrameColor)
+        cv2.imshow("rgb", frame)
+
+        if init_iter:
+            cv2.setMouseCallback("rgb", lambda *default: showPos(*default + tuple(frame.shape)))
+            init_iter = False
+
+        if cv2.waitKey(1) == ord('q'):
+            break
